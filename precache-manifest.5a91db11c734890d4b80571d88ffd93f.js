59c509b203c93cee2efa500e571076e00dee572e
Souiryuu <deetolentino24@gmail.com>
2022-12-11 18:08:19 +0800

    Updates

---
+++ precache-manifest.5a91db11c734890d4b80571d88ffd93f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c2a81109ac33fdbc43c",
+    "url": "/hello/static/css/main.77d5fb30.chunk.css"
+  },
+  {
+    "revision": "8c2a81109ac33fdbc43c",
+    "url": "/hello/static/js/main.8c2a8110.chunk.js"
+  },
+  {
+    "revision": "5a63e80967d755f43234",
+    "url": "/hello/static/js/1.5a63e809.chunk.js"
+  },
+  {
+    "revision": "17230c18a1f557561f20",
+    "url": "/hello/static/js/2.17230c18.chunk.js"
+  },
+  {
+    "revision": "57b5e17f53dcd51268e4",
+    "url": "/hello/static/js/runtime~main.57b5e17f.js"
+  },
+  {
+    "revision": "dae90d2d71b2bcdfbf158ed2782c6c25",
+    "url": "/hello/static/media/bg.dae90d2d.png"
+  },
+  {
+    "revision": "22e89e7d131dd4c62f60728cb71216cf",
+    "url": "/hello/index.html"
+  }
+];
